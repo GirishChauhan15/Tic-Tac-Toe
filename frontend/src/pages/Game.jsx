f70f43f9e1ceb9957417e123e93d8cdb7e1559c2
GirishChauhan15 <girishchauhan333@mail.com>
2025-07-10 21:01:00 +0530

    bug fix

--- frontend/src/pages/Game.jsx
+++ frontend/src/pages/Game.jsx
@@ -236,7 +236,7 @@ function Game() {
         </div>
       </div>
       <button
-        onClick={() => leaveRoomFn(roomCode, socket?.id)}
+        onClick={() => leaveRoomFn(roomCode, socket?.id,true)}
         className="absolute left-0 sm:left-10 bottom-2 bg-[#4c6ef5] m-0 px-5 py-2 rounded-full text-sm font-raleway font-medium hover:bg-[#3b5bdb] flex justify-center gap-1 items-center flex-wrap"
       >
         <LogOut className="size-4" /> Leave Room
